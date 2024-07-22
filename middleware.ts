119a28cacff2dacbff79814616a7fabf310b4f0f
Volodymyr Momot <86200333+mmtv-qa@users.noreply.github.com>
2024-07-22 16:15:28 +0300

    refactor: Improve authorization handling in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,14 @@ import { isAuthorized } from './app/lib/auth';
 import { env } from './app/config/env';
 
 export function middleware(request: NextRequest) {
+  if (env.API_TOKEN) {
+    return handleAuth(request, env.API_TOKEN);
+  }
+}
+
+function handleAuth(request: NextRequest, apiToken: string) {
   const actualAuthToken = request.headers.get('Authorization');
-  const expectedAuthToken = env.API_TOKEN;
+  const expectedAuthToken = apiToken;
 
   if (!isAuthorized({ actualAuthToken, expectedAuthToken })) {
     return CommonResponseFactory.buildUnauthorizedResponse();
