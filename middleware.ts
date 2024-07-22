dd7befb25f0c7b4435abe4411161637159aa200b
Volodymyr Momot <86200333+mmtv-qa@users.noreply.github.com>
2024-07-22 14:24:56 +0300

    refactor: Update middleware imports to use new file structure

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import type { NextRequest } from 'next/server';
-import { CommonResponseFactory } from './app/api/shared/response';
-import { isAuthorized } from './app/api/shared/auth';
+import { CommonResponseFactory } from './app/lib/response';
+import { isAuthorized } from './app/lib/auth';
 import { env } from './app/config/env';
 
 export function middleware(request: NextRequest) {
-  console.warn('middleware is running');
   const actualAuthToken = request.headers.get('Authorization');
   const expectedAuthToken = env.API_TOKEN;
 
