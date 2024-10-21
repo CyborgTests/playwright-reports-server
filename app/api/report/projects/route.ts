779b064e61de4b6ab968b5fd35627d7b6fe03ba4
Oleksandr Shevtsov <oshevtsov@bellum.ai>
2024-10-21 20:40:30 +0300

    fix: use dynamic projects api routes to avoid SSR

--- app/api/report/projects/route.ts
+++ app/api/report/projects/route.ts
@@ -1,6 +1,8 @@
 import { storage } from '@/app/lib/storage';
 import { withError } from '@/app/lib/withError';
 
+export const dynamic = 'force-dynamic'; // defaults to auto
+
 export async function GET() {
   const { result: projects, error } = await withError(storage.getReportsProjects());
 
