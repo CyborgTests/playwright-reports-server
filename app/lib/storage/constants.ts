7e4aaf68b7561d694071686ae3a890c2902a796e
Rostyslav Voitovych <r.voitovych@gmail.com>
2024-11-01 17:25:33 +0100

    move .tmp folder out of data folder

--- app/lib/storage/constants.ts
+++ app/lib/storage/constants.ts
@@ -9,6 +9,6 @@ export const REPORTS_BUCKET = `${DATA_PATH}/${REPORTS_PATH}`;
 
 export const DATA_FOLDER = path.join(process.cwd(), DATA_PATH);
 export const PW_CONFIG = path.join(process.cwd(), 'playwright.config.ts');
-export const TMP_FOLDER = path.join(DATA_FOLDER, '.tmp');
+export const TMP_FOLDER = path.join(process.cwd(), '.tmp');
 export const RESULTS_FOLDER = path.join(DATA_FOLDER, RESULTS_PATH);
 export const REPORTS_FOLDER = path.join(DATA_FOLDER, REPORTS_PATH);
