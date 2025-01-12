c13c3dd69dd1d796fca52224b793310ee9e10262
Shelex <11396724+Shelex@users.noreply.github.com>
2025-01-12 20:15:16 +0200

    feat: support report metadata file in routes, remove obsolete

--- app/api/report/trend/route.ts
+++ app/api/report/trend/route.ts
@@ -1,10 +1,6 @@
-import path from 'node:path';
-
 import { type NextRequest } from 'next/server';
 
 import { service } from '@/app/lib/service';
-import { isReportHistory, storage } from '@/app/lib/storage';
-import { parse } from '@/app/lib/parser';
 
 export const dynamic = 'force-dynamic'; // defaults to auto
 
@@ -15,22 +11,5 @@ export async function GET(request: NextRequest) {
 
   const latestReports = reports.slice(0, 20);
 
-  if (!latestReports.length || isReportHistory(latestReports.at(0))) {
-    return Response.json(latestReports);
-  }
-
-  // need to parse stats for each report if service cache not used
-  const latestReportsInfo = await Promise.all(
-    latestReports.map(async (report) => {
-      const html = await storage.readFile(path.join(report.project ?? '', report.reportID, 'index.html'), 'text/html');
-      const info = await parse(html as string);
-
-      return {
-        ...report,
-        ...info,
-      };
-    }),
-  );
-
-  return Response.json(latestReportsInfo);
+  return Response.json(latestReports);
 }
