f6c7221e6e8abcd67a1824063727a6f30f93ca99
Oleksandra Omer-Imereli <o.omer-imereli@globallogic.com>
2025-03-06 11:13:48 +0200

    fix pritier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from 'tailwindcss';
 
-import { heroui } from "@heroui/theme";
+import { heroui } from '@heroui/theme';
 
 const config = {
-  content: ['./app/**/*.{js,ts,tsx,mdx}', "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: ['./app/**/*.{js,ts,tsx,mdx}', './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
