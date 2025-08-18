3816c361a7102baeb82946e29fabba1ac0b293c1
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2025-08-18 18:12:25 +0300

    Added API tests based on @Vladyslava95 code
    Co-Authored-By: Vladyslava <71496479+Vladyslava95@users.noreply.github.com>

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { baseURL: 'http://localhost:3000', ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
---
+++ tests/api/generate.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+
+test('/api/result/upload should accept correct zip blob', async ({ request }) => {
+  const filePath = path.resolve(process.cwd(), './tests/testdata/blob.zip');
+  const zip = await readFile(filePath);
+  const newResult = await request.put('/api/result/upload', {
+    multipart: {
+      file: { name: 'blob.zip', mimeType: 'application/zip', buffer: zip },
+      project: 'Smoke',
+      tag: 'api-smoke',
+    }
+  });
+  expect(newResult.status()).toBe(200);
+
+  const body = await newResult.json();
+  expect(body.message).toBe('Success')
+  expect(body.data).toHaveProperty("resultID");
+  expect(body.data).toHaveProperty("createdAt");
+  expect(body.data.project).toBe("Smoke");
+  expect(body.data).toHaveProperty("size");
+  expect(body.data).toHaveProperty("sizeBytes");
+  expect(body.data).toHaveProperty("generatedReport");
+    
+});
