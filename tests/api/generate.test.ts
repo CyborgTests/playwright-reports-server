acff5041500e49b63a8222e52d59e3283dbaebdc
Vladyslava <71496479+Vladyslava95@users.noreply.github.com>
2025-08-19 17:08:09 +0300

    Add generate report test
    Add generate report test

--- tests/api/generate.test.ts
+++ tests/api/generate.test.ts
@@ -23,3 +23,38 @@ test('/api/result/upload should accept correct zip blob', async ({ request }) =>
   expect(body.data).toHaveProperty('sizeBytes');
   expect(body.data).toHaveProperty('generatedReport');
 });
+
+test('/api/report/generate should generate report', async ({ request }) => {
+  const filePath = path.resolve(process.cwd(), './tests/testdata/blob.zip');
+  const zip = await readFile(filePath);
+  const newResult = await request.put('/api/result/upload', {
+    multipart: {
+      file: { name: 'blob.zip', mimeType: 'application/zip', buffer: zip },
+      project: 'Smoke',
+      tag: 'api-smoke',
+    },
+  });
+
+  const resBody = await newResult.json();
+  const project = resBody.data?.project ?? resBody.results?.[0]?.project;
+  const resultID = resBody.data?.resultID ?? resBody.results?.[0]?.resultID;
+   expect(project).toBeTruthy();
+   expect(resultID).toBeTruthy();
+  
+  const newReport = await request.post('/api/report/generate', {
+    data: {
+      project: project,
+      resultsIds: [resultID],
+    },
+  });
+
+  const reportBody = await newReport.json();
+  const projectReport = reportBody.project ?? reportBody.metadata?.project;
+  console.log(reportBody);
+
+  expect(newReport.status()).toBe(200);
+  expect(reportBody.reportId).toBeTruthy();
+  expect(reportBody.reportUrl).toContain(`/api/serve/${project}/${reportBody.reportId}/`);
+  expect(projectReport).toBe(project);
+
+});
