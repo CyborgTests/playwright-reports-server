37e12c6094bb44307d9e34d10b5024b6a0e961ce
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2025-10-27 20:44:57 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   ],
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build && npm run start',
+    command: 'npm run dev',
     url: 'http://localhost:3000/api/info',
     reuseExistingServer: !process.env.CI,
   },
